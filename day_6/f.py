720b48495c847fe8479795e4c6617a63f3599436
Андрей Кулагин <72682155+Andrey2kulagin@users.noreply.github.com>
2022-09-01 21:48:12 +0300

    Create f.py

---
+++ day_6/f.py
@@ -0,0 +1,14 @@
+#Не смог решить эту задачу, алгоритм нашёл тут https://python.su/forum/topic/37511/
+n = int(input())
+res = 0
+all_date = [0] * (n + 1)
+if n != 0 and not (n % 2):
+    all_date[0] = 1
+    all_date[2] = 3
+    if n > 2:
+        all_date[4] = 11
+    for i in range(6, n + 1, 2):
+        all_date[i] = 4 * all_date[i - 2] - all_date[i - 4]
+
+    res = all_date[n]
+print(res)
