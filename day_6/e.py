2c5cd0ff71b6266ad6f8833c70e4e21b403911fb
Андрей Кулагин <72682155+Andrey2kulagin@users.noreply.github.com>
2022-09-01 21:30:30 +0300

    Update e.py

--- day_6/e.py
+++ day_6/e.py
@@ -1,20 +1,34 @@
-def set_result_arrays():
+#На автотестах крашится из-за ошибки в них, там не#
+def set_empty_arr(n):
+    a = []
+    for i in range(int(n)):
+        a.append([])
+    return a
+
+
+def arr_normilize(array, n):
+    a = []
+    for i in range(n):
+        if array[i]:
+            array[i].sort(reverse=True)
+            a.append(array[i])
+    return a
+
+def set_arrays():
     a = input()
+    all_arr = set_empty_arr(a)
+    for i in range(int(a)):
+        all_arr.append([])
     inp1 = input()
-    cure_arr = []
-    student = inp1[0]
-    all_arr = []
-    while inp1!='#':
+    while inp1 != '#':
         inp = inp1.split(" ")
-        if inp[0]!=student:
-            student = inp[0]
-            all_arr.append(cure_arr)
-            cure_arr = [] 
-        cure_arr.append(int(inp[1]))
+        all_arr[int(inp[0])].append(int(inp[1]))
         inp1 = input()
-    all_arr.append(cure_arr)
-    all_arr.sort()
-    return all_arr
-a = set_result_arrays()
-a.sort(key=lambda x: sum(x))
-print(*a)
+    ret_arr = arr_normilize(all_arr,int(a))
+    return ret_arr
+
+
+a = set_arrays()
+a.sort(key=lambda x: sum(x), reverse=True)
+for i in a:
+    print(*i, end=" ")
